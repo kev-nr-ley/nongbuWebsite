147c177cb7ccbf258e3fd7bd08856d5d9445ecac
discountratio <discountratio@gmail.com>
2023-03-19 17:05:52 +0700

    Update LogoLinks.jsx

--- src/components/LogoLinks.jsx
+++ src/components/LogoLinks.jsx
@@ -5,7 +5,7 @@ import InstagramLink from "@components/links/InstagramLink.jsx";
 import FacebookLink from "@components/links/FacebookLink.jsx";
 import FantuanLink from "@components/links/FantuanLink.jsx";
 import UberEatsLink from "@components/links/UberEatsLink.jsx";
-import SkipTheDishesLink from "@components/links/SkipTheDishesLink.jsx";
+// import SkipTheDishesLink from "@components/links/SkipTheDishesLink.jsx";
 export {
   EarLogoLink,
   NongbuLogoLink,
@@ -14,5 +14,5 @@ export {
   FacebookLink,
   FantuanLink,
   UberEatsLink,
-  SkipTheDishesLink,
+  // SkipTheDishesLink,
 };
